acc7a8689f115d62b249646f9e770f872ece8dab
noya-the-raccoon <noya.student@gmail.com>
2025-12-04 17:42:58 +0200

    huristic or something

--- showme.py
+++ showme.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 import matplotlib.pyplot as plt
 from drawing2 import Draw_distance
-from distance2 import distance
+from distance import distance
 from cgshop2026_pyutils.io import read_instance
 from cgshop2026_pyutils.geometry import FlippableTriangulation, draw_edges, Point 
 from cgshop2026_pyutils.schemas import CGSHOP2026Instance
